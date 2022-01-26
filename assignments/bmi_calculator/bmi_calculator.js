35b4de8a830e1362346c7b3af4b38e70f8dd7235
KundanSoftwareDeveloper <96870133+KundanSoftwareDeveloper@users.noreply.github.com>
2022-01-26 22:49:41 +0530

    Revert "Favourite movie"

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,12 +1,6 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
     // Write your code here
-    if (mass > 0 && height > 0){
-        return (mass/height**2);
-    }
-    else{
-        return "INVALID INPUT";
-    }
 }
 
 module.exports = BMICalculator;
